17b3bfb12f197145b7a73eeaaeda64a406b26cb5
Jullia <jullia.azevedo123@gmai.com>
2024-05-28 17:38:33 -0300

    Conecta com o backend utilizando o pacote axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
+import { getAllToDo } from "./utils/HandleApi";
 
 function App() {
+  const [toDo, setToDo] = useState([]);
+
+  useEffect(() => {
+    getAllToDo(setToDo);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <h1>TO-DO App</h1>
-
         <div className="top">
           <input type="text" placeholder="Adicione uma tarefa..." />
-
           <div className="add">Adicionar</div>
         </div>
 
         <div className="list">
-          <ToDo text="Hi" />
+        {toDo.map((item) => (
+            <ToDo key={item.id} text={item.text} />
+          ))}
         </div>
       </div>
     </div>
